3dd046cc811bfeecbaa0497c32fd40cf475ca803
Ivan Ayala Martinez <crazytotix@MacBook-Pro-de-Ivan.local>
2020-07-20 19:12:43 +0200

    FrontEnd improvement

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -6,6 +6,25 @@
     <title>Búsqueda</title>
 </head>
 <body>
-    {{$result}}
+<table>
+
+<table>
+    <tr>
+        <th>USERNAME</th>
+        <th>ID</th>
+        <th>BOT SCORE</th>
+        <th>FOLLOWERS</th>
+        <th>FRIENDS</th>
+    </tr>
+
+    <tr>
+         <td><?php echo $result ->scores['user']['screen_name'];?></td>
+         <td><?php echo $result -> scores['user']['id_str'];?></td>
+         <td><?php echo round($result -> scores['cap']['english'],2).'/'.round($result -> scores['cap']['universal'],2) ;?></td>
+         <td><?php echo count($result ->followers);?></td>
+         <td><?php echo count($result ->friends);?></td>
+    </tr>
+</table>
+   
 </body>
 </html>
